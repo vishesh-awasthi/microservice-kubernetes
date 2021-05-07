a9b91217143165c0dcf363347fd5819c29a20d58
visheshawasthi <vishesh.awasthi@paysafe.com>
2021-05-07 14:07:35 +0530

    Initial project commit

---
+++ country-service/src/main/java/com/visheshawasthi/client/StateClient.java
@@ -0,0 +1,35 @@
+package com.visheshawasthi.client;
+
+import com.visheshawasthi.model.State;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class StateClient {
+
+    private final RestTemplate restTemplate;
+    @Value("${client.state.url}")
+    private String stateClient;
+
+    public StateClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    /**
+     * Gets the states.
+     *
+     * @param countryCode represents country unique aplha3Code.
+     * @return {@linl List<State>}
+     */
+    public List<State> getStates(String countryCode) {
+        ResponseEntity<State[]> response = restTemplate.getForEntity(stateClient + countryCode, State[].class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return List.of(response.getBody());
+        }
+        return null;
+    }
+}
