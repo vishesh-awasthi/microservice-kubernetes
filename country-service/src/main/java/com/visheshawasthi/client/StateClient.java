8c22732fa807ea6397b284808275ff79a5edd5cc
visheshawasthi <visheshkawasthi>
2021-05-13 05:25:00 +0530

    passing state service host as env variable

--- country-service/src/main/java/com/visheshawasthi/client/StateClient.java
+++ country-service/src/main/java/com/visheshawasthi/client/StateClient.java
@@ -1,17 +1,21 @@
 package com.visheshawasthi.client;
 
 import com.visheshawasthi.model.State;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class StateClient {
 
+    @Autowired
     private final RestTemplate restTemplate;
+
     @Value("${client.state.url}")
     private String stateClient;
 
@@ -23,12 +27,12 @@ public class StateClient {
      * Gets the states.
      *
      * @param countryCode represents country unique aplha3Code.
-     * @return {@linl List<State>}
+     * @return {@link List<State>}
      */
     public List<State> getStates(String countryCode) {
         ResponseEntity<State[]> response = restTemplate.getForEntity(stateClient + countryCode, State[].class);
         if (response.getStatusCode().is2xxSuccessful()) {
-            return List.of(response.getBody());
+            return response.getBody()!= null ? List.of(response.getBody()) : Collections.emptyList();
         }
         return null;
     }
